aca1b68ee11d6a3a81ab8539ea687df656cdda77
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-30 22:18:21 +0200

    Create hook to detect the windows size

---
+++ hooks/useWindowsSize.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+export default function useWindowSize() {
+  const isClient = typeof window === "object";
+
+  function getSize() {
+    return {
+      width: isClient ? window.innerWidth : 0,
+      height: isClient ? window.innerHeight : 0
+    };
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    const removeWindowListener = async () => {
+      if (!isClient) {
+        return false;
+      }
+
+      function handleResize() {
+        setWindowSize(getSize());
+      }
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    };
+    removeWindowListener();
+  }, []);
+
+  const windowWidth = windowSize.width;
+  const isMobile = windowWidth >= 320 && windowWidth < 768;
+  const isTablet = windowWidth >= 768 && windowWidth < 1024;
+  const isLaptop = windowWidth >= 1024;
+
+  return { isMobile, isTablet, isLaptop };
+}
