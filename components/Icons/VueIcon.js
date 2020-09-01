2552a0cd2e69f375997f795b1b97c06684ab5bfc
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-09-02 00:20:20 +0200

    Add svg tech stack icons

---
+++ components/Icons/VueIcon.js
@@ -0,0 +1,8 @@
+export default function VueIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" {...props}>
+      <title>{'Vue.js icon'}</title>
+      <path d="M24 1.61h-9.94L12 5.16 9.94 1.61H0l12 20.78zM12 14.08L5.16 2.23h4.43L12 6.41l2.41-4.18h4.43z" />
+    </svg>
+  )
+}
