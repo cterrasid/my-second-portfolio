a4482efc12153d4896a9a14f1e494a0571b5dea8
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-09-02 22:32:56 +0200

    Create component for the titles

--- components/Tag/index.js
+++ components/Tag/index.js
@@ -1,10 +1,11 @@
 import { fonts, colors, breakpoints } from 'styles/theme'
+import { pxToRem } from 'styles/utils'
 
 export default function Tag({ name, children }) {
   return (
     <section>
       <span>{`<${name}>`}</span>
-      <>{children}</>
+      <div>{children}</div>
       <span>{`</${name}>`}</span>
 
       <style jsx>{`
@@ -16,31 +17,31 @@ export default function Tag({ name, children }) {
         span {
           color: ${colors.beigeMedium};
           font-family: ${fonts.secondary};
-          font-size: 12px;
+          font-size: ${pxToRem({ px: 12 })};
         }
 
         span:first-child {
           grid-area: 'open';
           align-self: flex-start;
-          margin-right: 5px;
+          margin-right: ${pxToRem({ px: 5 })};
         }
 
         span:last-child {
           grid-area: 'close';
           align-self: flex-end;
-          margin-left: 5px;
+          margin-left: ${pxToRem({ px: 5 })};
         }
 
         @media all and (min-width: ${breakpoints.tablet}px) {
           section:last-child {
             align-self: flex-end;
-            margin-right: 15px;
+            margin-right: ${pxToRem({ px: 15 })};
           }
         }
 
         @media all and (min-width: ${breakpoints.laptop}px) {
           span {
-            font-size: 18px;
+            font-size: ${pxToRem({ px: 18 })};
           }
         }
       `}</style>
